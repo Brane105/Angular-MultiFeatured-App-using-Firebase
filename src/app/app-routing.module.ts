c436894725c2461192007e5cdac747b284d7e06e
brane105 <brane6963@gmail.com>
2022-11-26 01:51:22 +0530

    new changes and components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainDashboardComponent } from './main/home/main-dashboard/main-dashboard.component';
+import { LoginComponent } from './main/login/login.component';
+import { RegisterComponent } from './main/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path:"",redirectTo:"/login",pathMatch:"full"},
+  {path:"login",component:LoginComponent},
+  {path:"register",component:RegisterComponent},
+  {path:"home",component:MainDashboardComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
